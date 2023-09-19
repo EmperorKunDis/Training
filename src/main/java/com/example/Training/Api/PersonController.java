f12880c90ae05b563b46db4a0736c73b0665dc5c
Martin Švanda <120254528+EmperorKunDis@users.noreply.github.com>
2023-09-19 21:44:53 +0200

    Little Improve

--- src/main/java/com/example/Training/Api/PersonController.java
+++ src/main/java/com/example/Training/Api/PersonController.java
@@ -5,9 +5,12 @@ import com.example.Training.Model.Person;
 import com.example.Training.Service.PersonService;
 import org.springframework.beans.factory.annotation.Autowire;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.UUID;
+
+@RequestMapping("api/v1/person")
 @RestController
 public class PersonController {
 
@@ -17,9 +20,28 @@ public class PersonController {
     public PersonController(PersonService personService) {
         this.personService = personService;
     }
-
     @PostMapping
-    public void addPerson(Person person) {
+    public void addPerson(@RequestBody Person person) {
         personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getAllPeople() {
+        return personService.getAllPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonByID(@PathVariable("id") UUID id) {
+        return personService.getPersonByID(id)
+                .orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deletePersonByID(@PathVariable("id") UUID id){
+        personService.deletePerson(id);
+    }
+    @PutMapping(path = "{id}")
+    public  void updatePerson(@PathVariable("id") UUID id, @RequestBody Person personToUpdate) {
+        personService.updatePerson(id, personToUpdate);
     }
 }
